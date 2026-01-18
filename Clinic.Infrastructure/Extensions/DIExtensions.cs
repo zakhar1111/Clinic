d868c4ca971927c2b7207c6c54e458d304c3d412
Seva Zakharchenko <vseva80@yandex.ru>
2026-01-17 22:59:52 -0600

    add DbContext

---
+++ Clinic.Infrastructure/Extensions/DIExtensions.cs
@@ -0,0 +1,34 @@
+﻿using Clinic.Infrastructure.Persistence;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Clinic.Infrastructure.Extensions;
+
+public static class DIExtensions
+{
+    public static void ApplyClinicMigrations(this IApplicationBuilder app)
+    {
+        using IServiceScope scope = app.ApplicationServices.CreateScope();
+        using ClinicDbContext context = scope.ServiceProvider.GetService<ClinicDbContext>();
+        context.Database.Migrate();
+    }
+    public static IServiceCollection AddClinicInfrastructure(
+        this IServiceCollection services,
+        IConfiguration config)
+    {
+        services.AddDbContext<ClinicDbContext>(options =>
+            options.UseSqlServer(
+                config.GetConnectionString("Default"),
+                sql =>
+                {
+                    sql.MigrationsHistoryTable("__EFMigrationsHistory", "deeds.message");
+                    sql.MigrationsAssembly("clinic.Infrastructure");
+                }
+                )
+            );
+
+        return services;
+    }
+}
---
+++ Clinic.Infrastructure/Persistence/ClinicDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Clinic.Infrastructure.Persistence;
+
+public class ClinicDbContext(DbContextOptions<ClinicDbContext> options)
+    : DbContext(options)
+{
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+        modelBuilder.ApplyConfigurationsFromAssembly(typeof(ClinicDbContext).Assembly);
+        modelBuilder.HasDefaultSchema("clinic");
+    }
+}
