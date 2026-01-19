e2895c09cc600559ee2972d5b3ee8298207e64ca
Seva Zakharchenko <vseva80@yandex.ru>
2026-01-19 13:28:45 -0600

    CQRS core

---
+++ Clinic.Shared/Messaging/IOperationHandler.cs
@@ -0,0 +1,7 @@
+﻿namespace Clinic.Shared.Messaging;
+
+public interface IOperationHandler<TRequest, TResult>
+    where TRequest : IRequest<TResult>
+{
+    Task<TResult?> HandleAsync(TRequest request, CancellationToken cancellationToken = default);
+}
---
+++ Clinic.Shared/Messaging/IRequest.cs
@@ -0,0 +1,3 @@
+﻿namespace Clinic.Shared.Messaging;
+
+public interface IRequest<TResult> { }
