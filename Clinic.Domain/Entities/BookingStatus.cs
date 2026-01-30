064e1a204e94c84e3ad6783fdc68f6edc7c3e126
Seva Zakharchenko <vseva80@yandex.ru>
2026-01-30 14:31:02 -0600

    specify Booking statuses

--- Clinic.Domain/Entities/BookingStatus.cs
+++ Clinic.Domain/Entities/BookingStatus.cs
@@ -5,3 +5,9 @@ public class BookingStatus
     public int Id { get; set; }
     public string Name { get; set; } = string.Empty;
 }
+
+// - Scheduled
+// - Confirmed ??
+// - Cancelled
+// - Completed
+// - 
