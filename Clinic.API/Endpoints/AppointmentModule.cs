66a3a6dfbbda72f50a1d5a8d9518a1630c6c27b1
Seva Zakharchenko <vseva80@yandex.ru>
2026-02-11 22:02:41 -0600

    ++ attach insurance for Appointment

--- Clinic.API/Endpoints/AppointmentModule.cs
+++ Clinic.API/Endpoints/AppointmentModule.cs
@@ -1,5 +1,6 @@
 ﻿using Carter;
 using Clinic.Application.Features.Appointment.Commands.PayForAppointmentCommand;
+using Clinic.Application.Features.Patient.Commands.AttachInsuranceCommand;
 using Clinic.Shared.Messaging;
 using Microsoft.AspNetCore.Mvc;
 
@@ -38,6 +39,29 @@ public class AppointmentModule
             .Produces(StatusCodes.Status400BadRequest);
 
         /// /appointments/{id}/insurance
+        app.MapPost(
+            "/appointments/{id}/insurance",
+            async (
+                int appointmentId,
+                [FromBody] AttachInsuranceCommand command,
+                [FromServices] OperationExecutor mediator,
+                CancellationToken ct) =>
+            {
+                command.AppointmentId = appointmentId;
+
+                var insuranceId = await mediator
+                    .ExecuteAsync<AttachInsuranceCommand, int>(command, ct);
+
+                return Results.Created(
+                    $"/appointments/{appointmentId}/insurance/{insuranceId}",
+                    new { insuranceId }
+                    );
+            })
+            .WithTags("Appointment")
+            .WithName("AttachInsurance")
+            .Produces<int>(StatusCodes.Status201Created)
+            .Produces(StatusCodes.Status400BadRequest);
+
         /// /appointments/{id}/diagnostics
         /// /appointments/{id}/notes
         /// /appointments /{ id}/ prescriptions
