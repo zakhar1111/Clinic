aa7664426833dcef4c573b3b8fa8d105df676a64
Seva Zakharchenko <vseva80@yandex.ru>
2026-02-25 22:35:26 -0600

    Dispatch events

---
+++ Clinic.Infrastructure/Messaging/IntegrationEventDispatcher.cs
@@ -0,0 +1,47 @@
+﻿using Clinic.Shared.Events;
+using System.Text.Json;
+
+namespace Clinic.Infrastructure.Messaging;
+
+public class IntegrationEventDispatcher(
+        IServiceProvider provider,
+        IEventRepository repository)
+{
+    private readonly IServiceProvider _provider = provider;
+    private readonly IEventRepository _repository = repository;
+
+
+    public async Task DispatchAsync(CancellationToken ct)
+    {
+        var events = await _repository.GetUnprocessedAsync(ct);
+
+        foreach (var integrationEvent in events)
+        {
+            var eventType = Type.GetType(integrationEvent.Type);
+
+            if (eventType is null)
+                continue;
+
+            var @event = JsonSerializer.Deserialize(
+                integrationEvent.Payload,
+                eventType
+            );
+
+            var handlerType = typeof(IIntegrationEventHandler<>)
+                .MakeGenericType(eventType);
+
+            var handler = _provider.GetService(handlerType);
+
+            if (handler is null)
+                continue;
+
+            var method = handlerType.GetMethod("HandleAsync");
+
+            await (Task)method!.Invoke(handler, new[] { @event!, ct });
+
+            integrationEvent.Processed = true;
+        }
+
+        await _repository.SaveChangesAsync(ct);
+    }
+}
