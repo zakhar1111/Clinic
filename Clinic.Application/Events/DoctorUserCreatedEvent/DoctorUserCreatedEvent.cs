192355d1724a7448ef6862c791d900a5bfa9ed47
Seva Zakharchenko <vseva80@yandex.ru>
2026-03-04 12:28:18 -0600

    Add Doctor creation event & handler

---
+++ Clinic.Application/Events/DoctorUserCreatedEvent/DoctorUserCreatedEvent.cs
@@ -0,0 +1,12 @@
+﻿using Clinic.Shared.Events;
+
+namespace Clinic.Application.Events.DoctorUserCreatedEvent;
+
+public class DoctorUserCreatedEvent 
+    : IIntegrationEvent
+{
+    public string UserId { get; set; } = default!;
+    public string Email { get; set; } = default!;
+    public string Name { get; set; } = default!;
+    public string Phone { get; set; } = default!;
+}
---
+++ Clinic.Application/Events/DoctorUserCreatedEvent/DoctorUserCreatedEventHandler.cs
@@ -0,0 +1,29 @@
+﻿using Clinic.Application.Repositories;
+using Clinic.Domain.Entities;
+using Clinic.Shared.Events;
+
+namespace Clinic.Application.Events.DoctorUserCreatedEvent;
+
+public class DoctorUserCreatedEventHandler(
+    IDoctorRepository doctorRepository)
+    : IIntegrationEventHandler<DoctorUserCreatedEvent>
+{
+    private readonly IDoctorRepository _doctorRepository = doctorRepository;
+
+    public async Task HandleAsync(
+        DoctorUserCreatedEvent @event,
+        CancellationToken ct)
+    {
+        var doctor = Doctor.Create(
+            userId: @event.UserId,
+            name: @event.Name,
+            phone: @event.Phone,
+            email: @event.Email,
+            bio: ""
+        );
+
+        await _doctorRepository.AddAsync(doctor, ct);
+        await _doctorRepository.SaveAsync(doctor, ct);
+    }
+}
+
