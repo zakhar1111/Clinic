314bd02bf09e4decf58bd42848ab21beb45558c0
Seva Zakharchenko <vseva80@yandex.ru>
2026-03-11 00:04:01 -0600

    ++  Pipeline Behavior

---
+++ Clinic.Shared/Pipeline/IPipelineBehavior.cs
@@ -0,0 +1,13 @@
+﻿namespace Clinic.Shared.Pipeline;
+
+
+public delegate Task<TResult> RequestHandlerDelegate<TResult>();
+
+public interface IPipelineBehavior<TRequest, TResult>
+{
+    Task<TResult> HandleAsync(
+        TRequest request,
+        RequestHandlerDelegate<TResult> next,
+        CancellationToken ct);
+}
+
