fcd5ce6f23e878a27006a8e29c9f88026c6e8168
Seva Zakharchenko <vseva80@yandex.ru>
2026-03-18 11:21:40 -0600

    Phone value object

---
+++ Clinic.Domain/Entities/Phone.cs
@@ -0,0 +1,16 @@
+﻿namespace Clinic.Domain.Entities;
+
+public record Phone
+{
+    public string Value { get; }
+
+    private Phone(string value) => Value = value;
+
+    public static Phone Create(string value)
+    {
+        if (string.IsNullOrWhiteSpace(value))
+            throw new ArgumentException("Invalid phone");
+
+        return new Phone(value);
+    }
+}
---
+++ Clinic.Infrastructure/Persistence/Convertors/PhoneConverter.cs
@@ -0,0 +1,14 @@
+﻿using Clinic.Domain.Entities;
+using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
+
+namespace Clinic.Infrastructure.Persistence.Convertors;
+
+public class PhoneConverter : ValueConverter<Phone, string>
+{
+    public PhoneConverter()
+        : base(
+            v => v.Value,
+            v => Phone.Create(v))
+    {
+    }
+}
